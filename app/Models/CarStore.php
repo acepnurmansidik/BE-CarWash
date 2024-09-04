d539c9b5ea118f0c1bdf1f576de0034a8c94c20b
acep nurman sidik <acepnurmansidik@gmail.com>
2024-09-04 23:11:43 +0700

    setup project

---
+++ app/Models/CarStore.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CarStore extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'is_open',
+        'is_full',
+        'city_id',
+        'addresss',
+        'phone_number',
+        'cs_name',
+    ];
+}
---
+++ app/Models/StoreService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class StoreService extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $fillable = ["car_service_id", 'car_store_id'];
+}
